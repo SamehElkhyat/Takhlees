969a06b51caa2e3387e443090c7aa20daa520637
SamehElkhyat <sameh.elkhyat@gmail.com>
2025-02-06 10:59:32 +0200

    first commit

--- src/Component/ClientsComponent/Orders/Orders.jsx
+++ src/Component/ClientsComponent/Orders/Orders.jsx
@@ -3,32 +3,27 @@ import React, { useEffect, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 
 const PendingOrders = () => {
-
   const [orders, setOrder] = useState([]);
   const [id, setid] = useState();
-  let [counter, setcounter] = useState(1)
-  const [Tokeen, setTokeen] = useState(null)
-
-
+  let [counter, setcounter] = useState(1);
+  const [Tokeen, setTokeen] = useState(null);
 
   const SendId = async () => {
-    console.log("iam here")
-    
-    const Tokken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjZhOTNjYTU5LWQ3MWUtNGVkMC04YzdhLWY5MmZjODY1ZTZmNCIsIkVtYWlsIjoiQnJva2VyQGdtYWlsLmNvbSIsImZ1bGxOYW1lIjoiQnJva2VyIiwicGhvbmVOdW1iZXIiOiI5NjM0LTk5MTk0IiwiSWRlbnRpdHkiOiIzMzMzMzMiLCJzZWN1cml0eVN0YW1wIjoiRFZFQUJNRDU2VlVFSTdONzY2REQ0Q1pPT1NKRTJER0YiLCJqdGkiOiIwZTkwNDc5Ni03YTM3LTQ3MTctYjYwZC03MmU0MDI5ZjJkNTMiLCJSb2xlIjoiQnJva2VyIiwiZXhwIjoxNzM5NDQyODkyLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MjY2IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzI2NiJ9.xlOr-vJB4pEIQgNetGVX7E0yDFUMZqgVR7uRbshqVys";
+    console.log("iam here");
 
+   
     if (id == 0) {
       console.log("Id Not Found");
     } else {
       try {
         console.log(id.ID);
-        
+
         const req = await axios.post(
           `https://user.runasp.net/api/Get-ID`,
           { ID: id },
           {
             headers: {
-              Authorization: `Bearer ${Tokken}`,
+              Authorization: `Bearer ${Tokeen}`,
             },
           }
         );
@@ -43,37 +38,27 @@ const PendingOrders = () => {
 
   const GetOrder = async () => {
     try {
-      const Tokken =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6ImUzMGU3YWYzLWYxNDktNGQ4ZC1iMDA3LWMxNWY0MmMyZGZhOSIsIkVtYWlsIjoiYWJkZWxtb2hzZW5AZ21haWwuY29tIiwiZnVsbE5hbWUiOiJhYmR1bGxhaCBtYWhtb3VkIGFiZGVsbW9oc2VuIiwicGhvbmVOdW1iZXIiOiIrMDU0ODQyMTU0ODU0IiwiSWRlbnRpdHkiOiI2NzMzNzA5ODQ4Iiwic2VjdXJpdHlTdGFtcCI6IjdCNks3U1RIV0QzNkRRSENXT0RJUVVXS01TVEpGTEk3IiwianRpIjoiMmQwYjZjZGItMDZmNy00ZDY5LTgxZmMtMjg5MzgzMWZjNGZkIiwiUm9sZSI6IlVzZXIiLCJleHAiOjE3Mzk3ODI4MzMsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcyNjYiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo3MjY2In0.bgY_OP6kGdlnXgocunUNQSECx_YwAfHmJWoQq1RPD58"
-    const res = await axios.get(
-      `https://user.runasp.net/api/Get-Orders`,
+      const res = await axios.get(
+        `https://user.runasp.net/api/Get-Orders`,
 
-      {
-        headers: {
-          Authorization: `Bearer ${Tokken}`,
-        },
-      }
-    );
-console.log(res);
-    setOrder
-    (res.data); 
-
+        {
+          headers: {
+            Authorization: `Bearer ${Tokeen}`,
+          },
+        }
+      );
+      setOrder(res.data);
     } catch (error) {
       console.log(error);
-      
     }
-
   };
-  const handleChangeId =(value)=>{
+  const handleChangeId = (value) => {
     setid(value);
-
-  }
-
-
+  };
 
   useEffect(() => {
-    const GetTokken=localStorage.getItem("Tokken")
-    setTokeen(GetTokken)    
+    const GetTokken = localStorage.getItem("Tokken");
+    setTokeen(GetTokken);
     GetOrder();
     SendId();
   }, [id]);
@@ -90,11 +75,14 @@ console.log(res);
           </tr>
         </thead>
         <tbody>
-          {orders.map((order,i) => (
-            <tr key={counter++} onClick={()=>{handleChangeId(order.id)}}>
-              {console.log(order.id)
-              }
-
+          {orders.map((order, i) => (
+            <tr
+              key={counter++}
+              onClick={() => {
+                handleChangeId(order.id);
+              }}
+            >
+              {console.log(order.id)}
 
               <td>{counter}</td>
               <td>{order.location}</td>
