4f7e7b2af1b2c95ac29f17b0ff11cceebefc1b90
SamehElkhyat <sameh.elkhyat@gmail.com>
2025-03-16 16:12:06 +0200

    first commit

--- src/Component/SignUpForCompany/SignUpForCompany.jsx
+++ src/Component/SignUpForCompany/SignUpForCompany.jsx
@@ -3,7 +3,7 @@ import "./SignUpForCompany.css";
 import SignUpBackground from "../SignIn/ships.png";
 import axios from "axios";
 import { useFormik } from "formik";
-import  Yup from "yup";
+import * as Yup from "yup";
 import toast, { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
 
@@ -14,7 +14,7 @@ const SignUpForCompany = () => {
       .required("البريد الإلكتروني مطلوب"),
     Password: Yup.string()
       .required("تأكيد كلمة المرور مطلوب")
-      .min(8, " تحتوي علي حروف و ارقام و حروف كبيره و صغيره")
+      .min(8, "و تحتوي علي حروف و ارقام و حروف كبيره و صغيره")
       .oneOf([Yup.ref("Confirm")], "كلمات المرور غير متطابقة"),
     fullName: Yup.string().required(" الاسم الكامل مطلوب"),
     Confirm: Yup.string()
