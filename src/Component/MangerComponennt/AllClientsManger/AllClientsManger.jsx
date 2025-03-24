67b191ea69ede78986b9b2d28a9255abcbb28313
SamehElkhyat <sameh.elkhyat@gmail.com>
2025-03-24 15:33:53 +0200

    first commit

--- src/Component/MangerComponennt/AllClientsManger/AllClientsManger.jsx
+++ src/Component/MangerComponennt/AllClientsManger/AllClientsManger.jsx
@@ -200,28 +200,6 @@ export default function AllClientsManger() {
                     {customer.statuOrder}
                   </Button>
                 </td>
-
-                <td>{customer.fullName}</td>
-                <td>{customer.email}</td>
-                <td>{customer.identity}</td>
-                <td>{customer.phoneNumber}</td>
-                <td>
-                  {customer.isBlocked ? (
-                    <button
-                      onClick={() => UnBlock(customer.email)}
-                      className="btn btn-success text-black"
-                    >
-                      فك الحظر
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => Block(customer.email)}
-                      className="btn btn-danger text-black"
-                    >
-                      حظر
-                    </button>
-                  )}
-                </td>
               </tr>
             ))}
           </tbody>
